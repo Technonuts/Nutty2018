c1e066ca62f8de7424b5939139467980559dd9f3
jhrenko311 <jhrenko311@yahoo.com>
2018-01-18 20:06:13 -0500

    we worked on the autonomous selection code

---
+++ src/org/usfirst/frc155/commands/AutoScaleRight.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc155.commands;
+
+import org.usfirst.frc155.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutoScaleRight extends CommandGroup {
+private double distance;
+private double shortDistance = 2;
+private double longDistance = 3;
+
+    public AutoScaleRight(int position) {
+    	if(position == 1 )
+    		distance = shortDistance;
+    	else distance = longDistance;
+    	addSequential(new DriveStraightDistance(10,.5));
+    	addParallel(new MoveLift(Robot.elevator.HIGHSCALEHEIGHT));
+    	addSequential(new TurnDriveAngle(-90,.5));
+    	addSequential(new DriveStraightDistance(distance,.25));
+    	//Add sequential "release" code 
+    	addSequential(new DriveStraightDistance(-1,.5));
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
