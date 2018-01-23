6a7d91239f16011bb42524134549a0c3a2a0b5ec
bfirst155 <bfirst155@10.0.0.69>
2018-01-22 19:18:18 -0500

    Jaymin

---
+++ src/org/usfirst/frc155/commands/CubeToScale.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc155.commands;
+
+import org.usfirst.frc155.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class CubeToScale extends CommandGroup {
+	
+	private int angle;
+	private double distance;
+	private double distance2, distance3;
+	
+    public CubeToScale(int side) {
+    	
+    	if(side==1) {
+    		angle =90;
+    	}
+    	else {
+    		angle=-90;
+    	}
+    
+    	addParallel(new MoveLift(Robot.elevator.HIGHSCALEHEIGHT));
+    	addSequential(new DriveStraightDistance(-distance,.5));
+    	addSequential(new TurnDriveAngle(angle,.5));
+    	addSequential(new DriveStraightDistance(distance2,.5));
+    	addSequential(new TurnDriveAngle(angle,.5));
+    	addSequential(new DriveStraightDistance(distance3,.5));
+    	addSequential(new TurnDriveAngle(angle,.5));
+    	addSequential(new DriveStraightDistance(1,.5));
+    	//Add sequential "release" code 
+    	addSequential(new DriveStraightDistance(-1,.5));
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
