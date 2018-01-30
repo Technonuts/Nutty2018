b4c3f6bfb3f7ce3ffba68c9c54fc7325dc82a783
nutty <nutty@10.0.0.188>
2018-01-29 19:42:14 -0500

    Added climbing code.

---
+++ src/org/usfirst/frc/team155/robot/commands/stopClimbing.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team155.robot.commands;
+
+import org.usfirst.frc.team155.robot.Robot;
+import org.usfirst.frc.team155.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class stopClimbing extends Command {
+
+    public stopClimbing() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires (Robot.elevator);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    		RobotMap.elevatorLeftHanger.setSpeed(0);
+    		RobotMap.elevatorRightHanger.setSpeed(0);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	RobotMap.elevatorLeftHanger.setSpeed(0);
+		RobotMap.elevatorRightHanger.setSpeed(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	RobotMap.elevatorLeftHanger.setSpeed(0);
+		RobotMap.elevatorRightHanger.setSpeed(0);
+    }
+}
