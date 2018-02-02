d77145c45324d0d7a9f6da40af80b0c0d1b2f721
bfirst155 <bfirst155@bfirst155-l1>
2018-02-01 20:05:29 -0500

    Lift

--- src/org/usfirst/frc/team155/robot/commands/manualMoveLift.java
+++ src/org/usfirst/frc/team155/robot/commands/manualMoveLift.java
@@ -2,32 +2,40 @@ package org.usfirst.frc.team155.robot.commands;
 
 import org.usfirst.frc.team155.robot.Robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class ManualLift extends Command {
+public class manualMoveLift extends Command {
 
-    public ManualLift() {
+    public manualMoveLift() {
         // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.elevator);
+         requires(Robot.elevator);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	new MoveLift(Robot.elevator.sliderValue(Robot.oi.switches));
+    	Robot.elevator.manualLift(Robot.oi.switches);  
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	  return Robot.elevator.onTarget();
+    	if(Robot.elevator.getHighLimit()&& Robot.oi.switches.getAxis(Joystick.AxisType.kY)>0) {
+    		return true;
+    	}
+    	else if(Robot.elevator.getLowLimit()&& Robot.oi.switches.getAxis(Joystick.AxisType.kY)<0 ) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 
     // Called once after isFinished returns true
@@ -38,5 +46,6 @@ public class ManualLift extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.elevator.holdLift();
     }
 }
